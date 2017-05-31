831477a2ae33a955dce1b62c01d8c429326bf706
Francisco <francisco.arca@hp.com>
2017-05-31 00:02:34 -0700

    web folder instead of build

---
+++ src/java/cart.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.*;
+import javax.servlet.RequestDispatcher;
+/**
+ *
+ * @author arca
+ */
+public class cart extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        ArrayList<String> ordertemp = new ArrayList<String>((ArrayList<String>) session.getAttribute("orders"));
+        String model = request.getParameter("model");
+        ordertemp.add(model);
+        session.setAttribute("orders",ordertemp);
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/cart.jsp");
+        rd.forward(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/cart.jsp
@@ -0,0 +1,43 @@
+<%-- 
+    Document   : cart
+    Created on : May 30, 2017, 12:52:46 PM
+    Author     : arca
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE>
+<html>
+<link rel="stylesheet" href="style.css">
+<script type="text/javascript" src="script.js"></script>
+<head>
+	<title>CoolFitted hats</title>
+</head>
+<body>
+	<center>
+		<div class='main'>
+			<div class='content'>
+				<div class='menubar'>
+				<div class='name'>
+					<a href="index" class="label compname">CoolFitted</a>
+				</div>
+				<div class='menu'>
+					<a href="index" class='label home'>Home</a>
+					<a href="newarr" class='label snap'>New Arrivals</a>
+					<a href="snapbacks" class='label snap'>Snapbacks</a>
+					<a href="strapbacks" class='label strap'>Strapbacks</a>
+					<a href="aboutus.html" class='label aboutus'>About Us</a>
+                                        <a href="cart.jsp" class='label cart active'>Cart</a>
+					</div>
+				</div>
+                                <div class='footercombo'>
+                                        <div class='combocontent' style="padding-top:1%; font-size: 20px">
+                                                CoolFitted<span style='font-size: 13px'>&reg;</span> 2017 &#8209; All Rights Reserved
+                                        </div>
+                                </div>
+				<br/>
+				<br/>
+			</div>
+		</div>
+	</center>
+</body>
+</html>
